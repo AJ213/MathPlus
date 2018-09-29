e9789064275d4300cc403d50c67941b7ff270072
Flootow <jacobwoodley2002@gmail.com>
2018-09-28 17:20:23 -0700

    The list of content is increasing

---
+++ MathPlus/Code/Statistics.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathPlus.Code
+{
+    class Statistics
+    {
+        //List<float> data = new List<float>(new float[] { 2, 7, 5});
+
+        public Sort sort;
+        public enum Sort
+        {
+            Assending,
+            Descending
+        }
+
+        public static List<float> SortInOrder(List<float> data, Sort sort)
+        {
+            List<float> newData = new List<float>() { data[0] };
+            data.RemoveAt(0);
+
+            foreach(float item in data)
+            {
+                for (int i = 0; i <= newData.Count; i++)
+                {
+                    if ((sort == Sort.Assending && item >= newData[i]) || (sort == Sort.Descending && item <= newData[i]))
+                    {
+                        newData.Insert(i, item);
+                        break;
+                    }
+                }
+            }
+            return newData;
+        }
+
+        public static float Mean(List<float> data)
+        {
+            return (data.Sum() / data.Count);
+        }
+        
+        public static float Median(List<float> data)
+        {
+            data = SortInOrder(data, Sort.Assending);
+            float index = (data.Count / 2);
+            return Mean(new List<float>() { (float) Math.Floor(index), (float) Math.Ceiling(index) });
+        }
+    }
+}
