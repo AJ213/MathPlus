487a358d748e617b859592746c6813426db35359
Flootow <jacobwoodley2002@gmail.com>
2018-09-28 20:02:40 -0700

    Helping with Trigonometry

--- MathPlus/Code/Trigonometry.cs
+++ MathPlus/Code/Trigonometry.cs
@@ -4,6 +4,11 @@ namespace MathPlus.Code
 {
     class Trigonometry
     {
+        public static double DegToRad(double angle)
+        {
+            return (angle * (Math.PI / 180.0f));
+        }
+
         public static double AreaTriangleSSS(double sideA, double sideB, double sideC)
         {
             double S = (sideA + sideB + sideC) / 2;
@@ -11,29 +16,20 @@ namespace MathPlus.Code
         }
         public static double AreaTriangleSAS(double sideA, double angleC, double sideB)
         {
-            return 0.5f * (sideA * sideB * Math.Sin(angleC));
+            return 0.5f * (sideA * sideB * Math.Sin(DegToRad(angleC)));
         }
         public static double AreaTriangleSSA(double sideA, double sideB, double angleA)
         {
-            double angleB = angleA * sideB / sideA;
-            double testSum = angleB - 180 + angleA;
-            if(testSum < 180)
-            {
-                //dothetwotrianglething
-                return;
-            }
-            else if(testSum > 180)
-            {
-                //ContinueCalculations?
-                return;
-            }
-            else if(Math.Sin(angleB) > 1)
+            double angleB = Math.Asin((sideB * Math.Sin(DegToRad(angleA))) / sideA);
+            double testSum = angleB - 180 + angleA; //If (testSum < 180) {2 Triangles} If (testSum > 180) {1 Triangle}
+
+            if(Math.Sin(angleB) > 1)
             {
                 return 0;
             }
             else
             {
-                return 0;
+                return ((sideA * sideB * Math.Sin(DegToRad(180 - angleA - angleB))) / 2);
             }
         }
         public static double AreaTriangleASA(double angleA, double sideB, double angleC)
